f945de1e467329664507520b582627e0551baec6
evgeniyvspb <volodin1981@yandex.ru>
2023-02-13 11:00:18 +0300

    nothing m$%%.к work

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите количество элементов в массиве: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string[] matrix = new string[number];
+for (int i = 0; (number - 1); i++)
+{
+    Console.WriteLine($"Введите {i + 1} значение массива: ");
+    matrix[i] = Console.ReadLine();
+}
+// int correct =0;
+// for (int i = 0; number-1; i++)
+// {
+//     if (matrix.Length[i]<4) correct++;
+// }
+// string[] matrixExit = new string[correct]; // матрица в которую мы запишем значение удовлетворяющие услвовию
+// int countForMatrixExit = 0;
+// for (int i = 0; number-1; i++)
+// {
+//     if (matrix.Length[i]<4) 
+//     {
+//         matrixExit[countForMatrixExit] = matrix.Length[i];
+//         countForMatrixExit++;
+//     }
+// }
+
+// PrintArray[matrix];
+// Console.Write("->");
+// PrintArray[matrixExit];
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i]);
+
+    }
+    Console.Write("]");
+}
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# finalTask
+Console.WriteLine("Введите количество элементов в массиве: ");
+int number = int.Parse(Console.ReadLine());
+string[] matrix = new int[number];
+for (int i = 0; number-1; i++)
+{
+    Console.WriteLine($"Введите {i+1} значение массива: ");
+    matrix[i] = Console.ReadLine();
+}
+int correct =0;
+for (int i = 0; number-1; i++)
+{
+    if (matrix.Length[i]<4) correct++;
+}
+string[] matrixExit = new int[correct]; // матрица в которую мы запишем значение удовлетворяющие услвовию
+int countForMatrixExit = 0;
+for (int i = 0; number-1; i++)
+{
+    if (matrix.Length[i]<4) 
+    {
+        matrixExit[countForMatrixExit] = matrix.Length[i];
+        countForMatrixExit++;
+    }
+}
+ 
+PrintArray[matrix];
+Console.Write("->");
+PrintArray[matrixExit];
+ 
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1) Console.Write(array[i]+",");
+        else Console.Write(array[i]);
+ 
+    }
+    Console.Write("]");
+}
