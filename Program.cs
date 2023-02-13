17506afa108e3c9a629add3a6ca7a325931d4fb4
evgeniyvspb <volodin1981@yandex.ru>
2023-02-13 11:49:18 +0300

    finally done

--- Program.cs
+++ Program.cs
@@ -1,30 +1,30 @@
 ﻿Console.WriteLine("Введите количество элементов в массиве: ");
 int number = Convert.ToInt32(Console.ReadLine());
 string[] matrix = new string[number];
-for (int i = 0; (number - 1); i++)
+for (int i = 0; i < number; i++)
 {
     Console.WriteLine($"Введите {i + 1} значение массива: ");
     matrix[i] = Console.ReadLine();
 }
-// int correct =0;
-// for (int i = 0; number-1; i++)
-// {
-//     if (matrix.Length[i]<4) correct++;
-// }
-// string[] matrixExit = new string[correct]; // матрица в которую мы запишем значение удовлетворяющие услвовию
-// int countForMatrixExit = 0;
-// for (int i = 0; number-1; i++)
-// {
-//     if (matrix.Length[i]<4) 
-//     {
-//         matrixExit[countForMatrixExit] = matrix.Length[i];
-//         countForMatrixExit++;
-//     }
-// }
+int correct = 0; // создаём массив с нужным количеством элементов
+for (int i = 0; i < number; i++)
+{
+    if (Size(matrix[i]) < 4) correct++;
+}
+string[] matrixExit = new string[correct]; // матрица в которую мы запишем значение удовлетворяющие услвовию
+int countForMatrixExit = 0;
+for (int i = 0; i<number; i++)
+{
+    if (Size(matrix[i]) < 4)
+    {
+        matrixExit[countForMatrixExit] = matrix[i];
+        countForMatrixExit++;
+    }
+}
 
-// PrintArray[matrix];
-// Console.Write("->");
-// PrintArray[matrixExit];
+PrintArray(matrix);
+Console.Write("->");
+PrintArray(matrixExit);
 
 void PrintArray(string[] array)
 {
@@ -36,4 +36,11 @@ void PrintArray(string[] array)
 
     }
     Console.Write("]");
+}
+
+int Size(string number)
+{
+    int size =0;
+    size = number.Length;
+    return size;
 }
